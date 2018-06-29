216f1e2604713c2795fcc17bc43eaff09dd30ada
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2018-06-29 12:23:22 +0100

    Refactor states into classes

--- quoptics/states.py
+++ quoptics/states.py
@@ -1,34 +1,86 @@
 import numpy as np
+from abc import ABC, abstractmethod
 from . import conf
 from scipy.special import factorial
 from . import operators as ops
 
-def fock(n, T=None):
+class _State(ABC):
+    """
+    Base state object
+    """
+    def __init__(self, T=None):
+        self.T = conf.T if T is None else T
+        self.data = np.empty(self.T)
+        super().__init__()
+        self._gen_data()
+
+    @abstractmethod
+    def _gen_data(self):
+        pass
+
+class Fock(_State):
     """
     Basis number states
     :param n: Number of the Fock state
     """
-    if T is None: T = conf.T
-    # Check n is non-negative int less than T
-    if not isinstance(n, (int, np.integer)) or n < 0:
-        raise ValueError("n must be a non-negative integer")
-    elif n >= T:
-        raise ValueError("n must be less than T")
-    # Construct Fock state
-    f = np.zeros(T)
-    f[n] = 1
-    return f
+    def __init__(self, n, T=None):
+        self.type = 'fock'
+        self.n = n
+        super().__init__(T=T)
 
-def coherent(alpha, T=None):
+    def _gen_data(self):
+        # Construct Fock state
+        self.data = np.zeros(self.T)
+        self.data[self.n] = 1
+
+class Coherent(_State):
     """
     Coherent states from analytic expression in Fock basis
     :param alpha: Complex number parametrising the coherent state
     """
-    if T is None: T = conf.T
-    state = [(alpha**n)/np.sqrt(factorial(n)) for n in range(T)]
-    state = np.array(state)
-    state = state * np.exp(-(np.abs(alpha)**2)/2)
-    return state
+    def __init__(self, alpha, T=None):
+        self.type = 'coherent'
+        self.alpha = alpha
+        super().__init__(T=T)
+
+    def _gen_data(self):
+        data = [(self.alpha**n)/np.sqrt(factorial(n)) for n in range(self.T)]
+        data = np.array(data)
+        data = data * np.exp(-(np.abs(self.alpha)**2)/2)
+        self.data = data
+
+class Squeezed(_State):
+    """
+    Squeezed states (single-mode) from analytic expression in Fock basis
+    :param z: Complex number that parametrises the squeezed state
+    """
+    def __init__(self, z, T=None):
+        self.type = 'squeezed'
+        self.z = z
+        super().__init__(T=T)
+
+    def _gen_data(self):
+        if self.z == 0:
+            # S(0) is the identity operator, so S(0)|0> = |0>
+            vacuum = Fock(0, T=self.T)
+            self.data = vacuum.data
+            return
+
+        def c(i):
+            """Coefficient of basis state |i>"""
+            n = i/2
+            cn = 1.0/np.sqrt(np.cosh(np.abs(self.z)))*np.sqrt(factorial(2*n))
+            cn *= 1.0/factorial(n)
+            cn *= (-self.z/(2.0*np.abs(self.z)))**n
+            cn *= np.tanh(np.abs(self.z))**n
+            return cn
+        state = [c(n) for n in range(self.T)]
+
+        # Squeezed states only have an even number of photons - set coefficients of
+        # odd Fock states to 0
+        zeros = np.zeros(len(state[1::2]))
+        state[1::2] = zeros
+        self.data = np.array(state)
 
 def coherent2(alpha, T=None):
     """
@@ -40,32 +92,6 @@ def coherent2(alpha, T=None):
     state = np.matmul(D, fock(0, T=T)) # Act on vacuum state with D(alpha)
     state = np.array(state) # Convert from np.matrix to np.array
     return state
-
-def squeezed(z, T=None):
-    """
-    Squeezed states (single-mode) from analytic expression in Fock basis
-    :param z: Complex number that parametrises the squeezed state
-    """
-    if T is None: T = conf.T
-    if z == 0:
-        # S(0) is the identity operator
-        return fock(0, T=T)
-
-    def c(i):
-        """Coefficient of basis state |i>"""
-        n = i/2
-        cn = 1.0/np.sqrt(np.cosh(np.abs(z)))*np.sqrt(factorial(2*n))
-        cn *= 1.0/factorial(n)
-        cn *= (-z/(2.0*np.abs(z)))**n
-        cn *= np.tanh(np.abs(z))**n
-        return cn
-    state = [c(n) for n in range(T)]
-
-    # Squeezed states only have an even number of photons - set coefficients of
-    # odd Fock states to 0
-    zeros = np.zeros(len(state[1::2]))
-    state[1::2] = zeros
-    return np.array(state)
 
 def squeezed2(z, T=None):
     """
