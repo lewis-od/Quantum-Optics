3357f89fe6d5ec56cdc5ad19fe11abd3db6628db
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2018-07-03 13:31:35 +0100

    Re-write neural network to be more transparent

--- network.py
+++ network.py
@@ -1,109 +1,78 @@
+import os
 import numpy as np
 import tensorflow as tf
-import os
-import sys
 
-# Number of categories to classify states into
-N_TYPES = 4
+sess = tf.Session()
 
-## Define the network
+## Define the network graph
 x = tf.placeholder(dtype=tf.float32, shape=[None, 200])
 y = tf.placeholder(dtype=tf.int32, shape=[None])
 
-layer1 = tf.contrib.layers.fully_connected(x, 100, tf.nn.relu)
-layer2 = tf.contrib.layers.fully_connected(layer1, 50, tf.nn.relu)
-layer3 = tf.contrib.layers.fully_connected(layer2, 25, tf.nn.relu)
-logits = tf.contrib.layers.fully_connected(layer3, N_TYPES, tf.nn.sigmoid)
+hidden1 = tf.layers.dense(x, 100, activation=tf.nn.relu, name="hidden1")
+hidden2 = tf.layers.dense(hidden1, 50, activation=tf.nn.relu, name="hidden2")
+hidden3 = tf.layers.dense(hidden2, 25, activation=tf.nn.relu, name="hidden3")
 
-# Use softmax loss function (mutually exclusive categories)
-loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits))
+logits = tf.layers.dense(hidden3, 4, activation=tf.nn.relu, name="logits")
 
-# Use ADAM algorithm for optimisation
+prediction = tf.argmax(logits, 1, name="prediction")
+loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y,
+    logits=logits, name="loss"))
+
 train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
 
-correct_pred = tf.argmax(logits, 1)
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+sess.run(tf.global_variables_initializer())
 
-## Load the training data
-def load_data(data_dir, fname):
-    train_path = os.path.join(data_dir, fname)
-    train_file = np.load(train_path)
-
-    states = train_file['states']
-    labels = train_file['labels']
-
-    # Use |c_n| as training input, since physics depends on |c_n|^2
+## Helper functions for handling data
+def load_data(dir, name):
+    """Loads a .npz file from the data directory"""
+    if name.split('.')[-1] != 'npz':
+        name += '.npz'
+    f = os.path.join(dir, name)
+    # Load the data from the file
+    d = np.load(f)
+    states = d['states']
+    labels = d['labels']
+    # Format the data for the network
     states = np.abs(states)
-    states[np.isnan(states)] = 0
-
+    states[np.isnan(states)] = 0.0
     return states, labels
 
-# Get a batch from the training data
-def fetch_batch(batch_index, batch_size):
-    batch_states = states[batch_index*batch_size:(batch_index+1*batch_size)]
-    batch_labels = labels[batch_index*batch_size:(batch_index+1*batch_size)]
-    return batch_states, batch_labels
+def fetch_batch(data, labels, n, b_size):
+    """Returns the nth batch of size b_size from data and labels"""
+    batch_data = data[n*b_size:(n+1)*b_size]
+    batch_labels = labels[n*b_size:(n+1)*b_size]
+    return batch_data, batch_labels
 
-# Shuffle states and labels
-def shuffle_data(states, labels):
-    new_states = np.empty(states.shape, dtype=states.dtype)
-    new_labels = np.empty(labels.shape, dtype=labels.dtype)
-    perm = np.random.permutation(len(labels))
-    for old, new in enumerate(perm):
-        new_states[new] = states[old]
-        new_labels[new] = labels[old]
-    return new_states, new_labels
-
-# Train the network
-def train(states, labels):
-    for epoch in range(501):
-        for b_n in range(5):
-            batch = fetch_batch(b_n, 1000)
-            _, accuracy_val = sess.run([train_op, accuracy],
-                feed_dict={ x: batch[0], y: batch[1] })
-        # states, labels = shuffle_data(states, labels)
-        if epoch % 10 == 0:
-            print("Epoch: {}".format(epoch))
-
-# Test the network against the test data
-def test(data_dir):
-    test_states, test_labels = load_data(data_dir, "test.npz")
-
-    predicted = sess.run([correct_pred], feed_dict={x: test_states})[0]
-    predicted = np.array(predicted)
-
-    loss_val = sess.run(loss, feed_dict={x: test_states, y: test_labels})
-    print("Loss value is: {}".format(loss_val))
-
-    n_correct = np.sum(predicted == test_labels)
-    test_accuracy = float(n_correct) / float(len(test_labels))
-
-    conf_mat = sess.run(tf.confusion_matrix(test_labels, predicted))
-
-    print("{} states out of {} correctly classified".format(n_correct, len(test_labels)))
-    print("Test accuracy is: {}".format(test_accuracy))
-    print("Confusion matrix:")
-    print(conf_mat)
-
-def classify(state):
-    data = np.abs(state.data)
-    data[np.isnan(data)] = 0.0
-    types = ['fock', 'coherent', 'squeezed', 'cat']
-    index = sess.run(correct_pred, feed_dict={x: [data]})[0]
-    return types[index]
-
-# Initialise the session
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
 cur_dir = os.path.abspath(os.path.join(__file__, os.pardir))
 data_dir = os.path.join(cur_dir, "data")
 
-## Train network
-# states, labels = load_data(data_dir, "train.npz")
-# train(states, labels)
+## Train the network
+def train():
+    train_states, train_labels = load_data(data_dir, "train")
+    for epoch in range(500):
+        if epoch % 50 == 0:
+            print("Epoch: {}".format(epoch))
+        for b_n in range(5):
+            batch = fetch_batch(train_states, train_labels, b_n, 1000)
+            sess.run(train_op, feed_dict={x: batch[0], y: batch[1]})
+    print("Training completed.")
 
-## Use trained network
-saver = tf.train.Saver()
-saver.restore(sess, os.path.join(cur_dir, "weights", "model.ckpt"))
+## Test the network
+def test():
+    test_states, test_labels = load_data(data_dir, "test")
+    test_predictions = sess.run(prediction, feed_dict={ x: test_states })
+    n_correct = np.sum(test_predictions == test_labels)
+    accuracy = float(n_correct)/len(test_labels)
+    conf_mat = sess.run(tf.confusion_matrix(test_labels, test_predictions))
+    print("Network classifed {}/{} states correctly ({}%)".format(n_correct, len(test_labels), accuracy*100))
+    print("Confusion matrix:")
+    print(conf_mat)
 
-test(data_dir)
+def classify(data):
+    """Classifies data using the trained network"""
+    pred = sess.run(prediction, feed_dict={x: [data]})
+    return pred[0]
+
+if __name__ == '__main__':
+    train()
+    test()
