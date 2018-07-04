3e4d06b96c61eb9b85aae97a6eead32cd4b46406
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2018-07-04 15:05:45 +0100

    Add basic unit tests

---
+++ run_tests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python -m unittest discover -s test -p "utest_*.py"
---
+++ test/states/utest_states.py
@@ -0,0 +1,35 @@
+import unittest
+import numpy as np
+from quoptics.states import Coherent, Fock
+
+class TestStates(unittest.TestCase):
+
+    def setUp(self):
+        # Things tested here are implemented by the State base class, so
+        # only need to test one of the State subclasses
+        self.state = Coherent(1j)
+
+    def testChangeParameter(self):
+        old = list(self.state.data)
+        self.state.alpha = 1.0
+        self.assertNotEqual(old, list(self.state.data))
+
+    def testChangeTruncation(self):
+        self.state.T = 100
+        self.assertEqual(len(self.state.data), 100)
+
+    def testChangeAnalytic(self):
+        old = list(self.state.data)
+        self.state.analytic = False
+        self.assertNotEqual(old, list(self.state.data))
+
+    def testInnerProduct(self):
+        psi = Fock(4)
+        c = self.state.inner_prod(psi)
+        self.assertEqual(self.state.data[4], c)
+
+    def testNorm(self):
+        norm = self.state.norm()
+        inner = self.state.inner_prod(self.state)
+        self.assertEqual(norm, inner)
+        self.assertTrue(norm <= 1)
